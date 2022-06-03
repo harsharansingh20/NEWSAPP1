9a49a093227c26ffd7f65ac1a75010e79e9524d3
harsharan  Singh <harsharansingh20@gmail.com>
2022-06-04 00:35:23 +0530

    adding cards

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -4,10 +4,10 @@ export default class Newsitem extends Component {
   render() {
     return (
       <div>
-        <div className="card mb-3" style="max-width: 5px;">
+        <div className="card mb-3" style={{width: "18rem"}}>
           <div className="row g-0">
             <div className="col-md-4">
-              <img src="..." className="img-fluid rounded-start" alt="..." />
+              <img src="..." className="img-fluid rounded-start" alt="..."/>
             </div>
             <div className="col-md-8">
               <div className="card-body">
