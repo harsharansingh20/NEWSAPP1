447d24e819f827d2d21b3d980e493cc6522affbc
harsharan  Singh <harsharansingh20@gmail.com>
2022-06-06 23:59:38 +0530

    j

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export default class News extends Component {
   render() {
     return (
-      <div className='container my-3'>
-         <h2>News-Monkey  Top Headlines</h2> 
-        <div className='row'>
-        <div className='col-md-4' >
-        <NewsItem title ="hello" />
+      <div className="container my-3">
+        <h2 className="heading">News-Monkey Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4 my-4">
+            <NewsItem title="1"/>
+          </div>
+          <div className="col-md-4 my-4">
+            <NewsItem  title="2" />
+          </div>
+          <div className="col-md-4 my-4">
+            <NewsItem  title="3"/>
+          </div>
+          <div className="col-md-4 my-4">
+            <NewsItem title="4" />
+          </div>
+          <div className="col-md-4 my-4">
+            <NewsItem  title="5"/>
+          </div>
+          <div className="col-md-4 my-4">
+            <NewsItem  title="5"/>
+          </div>
         </div>
-        <div className='col-md-4' >
-        <NewsItem title ="hello" />
-        </div>
-        <div className='col-md-4' >
-        <NewsItem title ="hello" />
-        </div>
-        <div className='col-md-4' >
-        <NewsItem title ="hello" />
-        </div>
-        <div className='col-md-4' >
-        <NewsItem title ="hello" />
-        </div>
-        <div className='col-md-4' >
-        <NewsItem title ="hello" />
-        </div>
-        </div>        
-        </div>
-    )
+      </div>
+    );
   }
 }
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -2,28 +2,19 @@ import React, { Component } from "react";
 
 export default class Newsitem extends Component {
   render() {
-    let {title,description} = this.props;
+    let { title, description } = this.props;
     return (
-      <div>
-        <div className="card mb-4"style={{width: "18rem"}}>
-          <div className="row g-0">
-            <div className="col-md-4">
-              <img src="..." className="img-fluid rounded-start" alt="..."/>
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">
-                  {description}
-                </p>
-                <p className="card-text">
-                  <small className="text-muted">Last updated 3 mins ago</small>
-                </p>
-              </div>
-            </div>
+    <div>
+
+      <div class="card" style={{width: "18rem"}}>
+        <img src="..." class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">{title}</h5>
+            <p class="card-text">{description}</p>
+            <a href="/newsdeail" class="btn btn-primary">Go somewhere</a>
           </div>
-        </div>
       </div>
+    </div>
     )
   }
 }
