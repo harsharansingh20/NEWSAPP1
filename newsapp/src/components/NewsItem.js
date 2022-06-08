a91342e07edf83694e1ea02dd80f2cfc4fd21d34
harsharan  Singh <harsharansingh20@gmail.com>
2022-06-09 00:14:35 +0530

    9 june

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -9,7 +9,7 @@ export default class Newsitem extends Component {
       <div>
 
       <div className="card" style={{width: "18rem"}}>
-        <img src={imageurl} className="card-img-top" alt="..."/>
+        <img src={!imageurl?"https://images.unsplash.com/photo-1654686174962-5caf1d99bb6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80":imageurl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
