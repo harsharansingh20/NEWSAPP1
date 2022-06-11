0b7b86fae22418318d93a936b6d1e531ff395f27
harsharan  Singh <harsharansingh20@gmail.com>
2022-06-11 20:13:56 +0530

    finding some bugs

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -18,12 +18,69 @@ export default class News extends Component {
 
   async componentDidMount(){
     console.log("cdm");
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=499d49664ef94b21b1dc3d9d3283fc2d";
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=499d49664ef94b21b1dc3d9d3283fc2d&page=1";
     let data =  await fetch(url);
     let parsedData = await data.json()
     console.log(parsedData);
     this.setState({articles:parsedData.articles})
     }
+
+  handlePrevClick= async ()=>{
+
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=499d49664ef94b21b1dc3d9d3283fc2d"&page=${this.state.page - 1}`;
+    let data =  await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    
+
+    this.setState({
+      page: this.state.page - 1,
+      articles:parsedData.articles
+    })
+
+    
+      // console.log("Previous");
+      // let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=499d49664ef94b21b1dc3d9d3283fc2d”&page=${this.state.page - 1}&pageSize=20`;
+      // let data = await fetch(url);
+      // let parsedData = await data.json()
+      // console.log(parsedData);
+      // this.setState({
+      //     page: this.state.page - 1,
+      //     articles: parsedData.articles
+      // })
+  
+
+  }
+
+  handleNextClick= async ()=>{
+
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=499d49664ef94b21b1dc3d9d3283fc2d"&page=${this.state.page + 1}`;
+    let data =  await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    
+
+    this.setState({
+      page: this.state.page + 1,
+      articles:parsedData.articles
+    })
+
+    
+      // console.log("Next");
+      // if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+      // }
+      // else {
+      //     let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=499d49664ef94b21b1dc3d9d3283fc2d"&page=${this.state.page + 1}&pageSize=20`;
+      //     let data = await fetch(url);
+      //     let parsedData = await data.json()
+      //     console.log(parsedData);
+      //     this.setState({
+      //         page: this.state.page + 1,
+      //         articles: parsedData.articles
+      //     })
+      
+  }
+  
 
   render() {
     return (
@@ -32,14 +89,14 @@ export default class News extends Component {
         <div className="row">
         {this.state.articles.map((element)=>{
         return  <div className="col-md-4 my-4"key={element.url} >
-          <NewsItem  title={element.title?element.title.slice(0, 44):""} description={element.description?element.description.slice(0, 88):""} imageurl={element.urlToImage} newsurl={element.url} />
+          <NewsItem  title={element.title?element.title.slice(0, 90):""} description={element.description?element.description.slice(0, 88):""} imageurl={element.urlToImage} newsurl={element.url} />
         </div>
         })}
        </div>
 
 <div className="container d-flex justify-content-between">
-<button type="button " class="btn btn-dark">Previous &larr; </button>
-<button type="button " class="btn btn-dark">Next &rarr;</button>
+<button disabled= {this.state.page<=1}  type="button " className="btn btn-dark" onClick={this.handlePrevClick}>&larr;  Previous  </button>
+<button type="button " className="btn btn-dark" onClick={this.handleNextClick} >Next &rarr;</button>
 
 
 </div>
--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -13,7 +13,7 @@ export default class Newsitem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            {/* <a href={newsurl} target="_blank" className="btn btn-outline-dark">Read More</a> */}
+            <a href={newsurl} target="_blank" className="btn btn-outline-dark">Read More</a>
           </div>
       </div>
     </div>
